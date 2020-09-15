05504f20a72d11eef2cfcf40a49e7f0abb1b38d5
Andrew Aponte <andrew.aponte@xcentium.com>
2020-09-14 23:41:25 -0400

    adding the ability to specify an offset for the scrollTo directive

--- src/lib/directives/scroll-to.directive.ts
+++ src/lib/directives/scroll-to.directive.ts
@@ -1,7 +1,9 @@
 /*
 
     Scroll-To Directive
-        - Provides an easy way to "smooth" scroll to an element, if the browser supports it. Pass in a CSS selector for the element you want to scroll towards.
+        - Provides an easy way to "smooth" scroll to an element, if the browser supports it.
+        - You can pass in a CSS selector for the element you want to scroll towards.
+        - Or you can pass in an options object. Check out the ScrollOptions interface for more information.
 
         [Example]
 
@@ -9,18 +11,30 @@
 
 */
 
-import { AfterViewInit, Directive, ElementRef, Input, OnDestroy } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { ScrollOptions } from '../data/types';
 
 @Directive({
     selector: '[scrollTo]',
 })
-export class ScrollToDirective implements AfterViewInit, OnDestroy {
-    @Input('scrollTo') cssString: string;
+export class ScrollToDirective implements OnInit, AfterViewInit, OnDestroy {
+    @Input('scrollTo') input: string | ScrollOptions;
 
     elem: HTMLElement;
+    target: string;
+    offset = 0;
 
     constructor(private elementRef: ElementRef) {
         this.elem = this.elementRef.nativeElement;
+    }
+
+    ngOnInit(): void {
+        if (this.input && typeof this.input === 'object') {
+            this.target = this.input.target;
+            this.offset = this.input.offset;
+        } else {
+            this.target = this.input as string;
+        }
     }
 
     ngAfterViewInit(): void {
@@ -33,14 +47,14 @@ export class ScrollToDirective implements AfterViewInit, OnDestroy {
 
     onScroll = (): void => {
         const smoothScrollSupport = 'scrollBehavior' in document.documentElement.style;
-        const target = document.querySelector(this.cssString);
+        const target = document.querySelector(this.target);
 
         if (!target) {
-            console.warn(`Unable to scroll the page. An element with matching the CSS string "${ this.cssString }" could not be found.`);
+            console.warn(`Unable to scroll the page. An element with matching the CSS string "${ this.target }" could not be found.`);
             return;
         }
 
-        const yCoord = target.getBoundingClientRect().top + window.scrollY;
+        const yCoord = target.getBoundingClientRect().top + window.scrollY + this.offset;
 
         if (smoothScrollSupport) {
             window.scrollTo({
